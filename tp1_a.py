2cfc7d977026772b86cff7531e4188115d2bb3cb
Juan-Pedro-Kessler <juanpedrokessler@gmail.com>
2024-08-14 15:41:32 -0300

    primero

--- tp1_a.py
+++ tp1_a.py
@@ -1,12 +1,16 @@
-print("ingrese el lado a")
-a=int(input())
-print("ingrese el lado b")
-b=int(input())
-print("ingrese el lado c")
-c=int(input())
-if(a==b and a==c):
-    print("el triangulo es esquilatero")
-if(a == b or a == c or b == c):
-    print("el triangulo es isoceles")
-if(a!=b and a!=c and b!=c):
-    print("el triangulo es escaleno")
+#Realizar un programa que pida los tres lados de un triángulo e indique el tipo de 
+##triángulo que es según sus lados: Equilátero (tres lados iguales), Isósceles (dos 
+#lados iguales) o Escaleno (tres lados distintos). 
+print("Ingrese el lado a")
+a = int(input())
+print("Ingrese el lado b")
+b = int(input())
+print("Ingrese el lado c")
+c = int(input())
+
+if (a == b and a == c):
+    print("El triángulo es equilátero")
+elif (a == b or a == c or b == c):
+    print("El triángulo es isósceles")
+else:
+    print("El triángulo es escaleno")
